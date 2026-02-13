320c33fb743d38d3fe349cb4478f86b6b687849a
Miguel Trincheiras <mmtrincheiras@iscsp.ulisboa.pt>
2026-02-13 10:30:10 +0000

    firebase api details

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "your-api-key",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  appId: "your-app-id",
+  apiKey: "AIzaSyAJlHQ7-X7DnwBErI01CFiUoQ6iUDiTuko",
+  authDomain: "iscsptasks-3bb98.firebaseapp.com",
+  projectId: "iscsptasks-3bb98",
+  appId: "1:161280679161:web:3325ac3f85ad178d5e1628",
 };
